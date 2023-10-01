527c82dc37521e83c1e9226a4311a72d503fdbe8
= <=>
2023-10-01 00:14:10 -0300

    Exportando e Importando um Componente

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,8 +1,14 @@
+import Quadrado, { Circle as Circulo } from "@/components/Geometry";
+
 function Page() {
   return <>
     <h1 className="font-bold text-2xl">Olá, mundo!</h1>
     <h3>Algum outro texto!</h3>
+
+    <Quadrado />  
+    <Circulo />
   </>;
+  
 }
 
 export default Page;
