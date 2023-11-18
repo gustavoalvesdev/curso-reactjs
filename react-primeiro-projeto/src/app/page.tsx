30d67eba2b44371f913638fc30360bad0e2b1449
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-11-18 12:59:38 -0300

    Regrsa gerais do JSX

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { GeoForm } from "@/components/GeoForm";
+import { Person } from "@/components/Person";
 
 function Page() {
   return <>
     <h1 className="font-bold text-2xl">Olá, mundo!</h1>
     <h3>Algum outro texto!</h3>
 
-    <GeoForm />
+    <Person />
   </>;
   
 }
---
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -0,0 +1,21 @@
+export const Person = () => {
+    return (
+        <>
+            <h1>Elon Musk</h1>
+            <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTam7dK4a6nCf6yhn8M0RPFBa1yIbNrD_9EUJ3NNEk7-o0KGCIZ" 
+                alt="Elon Musk"
+                className="w-40"
+            />
+            <h2>Suas Empresas:</h2>
+            <ul>
+                <li>Fundador, diretor executivo e diretor técnico da <strong>SpaceX</strong></li>
+                <li>CEO da <strong>Telas</strong></li>
+                <li>CEO do <strong>Twitter</strong></li>
+                <li>Vice-presidente da <strong>OpenAI</strong></li>
+                <li>Fundador e CEO da <strong>Neuralink</strong></li>
+                <li>Co-fundador e presidente da <strong>SolarCity</strong></li>
+            </ul>
+        </>
+    );
+}
