0162be8b97d2f4afa67e090e1c0c263c772ef80b
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-11-18 12:45:55 -0300

    Usando vários componentes

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,12 +1,11 @@
-import Quadrado, { Circle as Circulo } from "@/components/Geometry";
+import { GeoForm } from "@/components/GeoForm";
 
 function Page() {
   return <>
     <h1 className="font-bold text-2xl">Olá, mundo!</h1>
     <h3>Algum outro texto!</h3>
 
-    <Quadrado />  
-    <Circulo />
+    <GeoForm />
   </>;
   
 }
