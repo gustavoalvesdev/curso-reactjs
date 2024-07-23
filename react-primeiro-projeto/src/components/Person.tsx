658d0f55d8f6724ff258f7d388dc1edbda84dc74
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-22 21:52:58 -0300

    JSX e variáveis, funções e objetos

--- react-primeiro-projeto/src/components/Person.tsx
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -1,20 +1,31 @@
+const getWeekDay = (today: Date) => {
+    return new Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(today);
+}
+
 export const Person = () => {
+    
+    const data = {
+        name: 'Elon Musk',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTam7dK4a6nCf6yhn8M0RPFBa1yIbNrD_9EUJ3NNEk7-o0KGCIZ',
+        roles: [
+            'CEO da Tesla',
+            'CEO da SpaceX'
+        ]
+    }
+
     return (
         <>
-            <h1>Elon Musk</h1>
+            <h1 style={{ color: 'red', fontSize: '30px' }}>{data.name} - {getWeekDay(new Date())}</h1>
             <img 
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTam7dK4a6nCf6yhn8M0RPFBa1yIbNrD_9EUJ3NNEk7-o0KGCIZ" 
-                alt="Elon Musk"
+                src={data.avatar}
+                alt={data.name}
                 className="w-40"
             />
             <h2>Suas Empresas:</h2>
             <ul>
                 <li>Fundador, diretor executivo e diretor técnico da <strong>SpaceX</strong></li>
-                <li>CEO da <strong>Telas</strong></li>
-                <li>CEO do <strong>Twitter</strong></li>
-                <li>Vice-presidente da <strong>OpenAI</strong></li>
-                <li>Fundador e CEO da <strong>Neuralink</strong></li>
-                <li>Co-fundador e presidente da <strong>SolarCity</strong></li>
+                <li>{data.roles[0]}</li>
+                <li>{data.roles[1]}</li>
             </ul>
         </>
     );
