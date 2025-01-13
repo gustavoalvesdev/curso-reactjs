aa8095ccdb14ae7b216cbea08ed6e3e53addea8c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-01-13 18:45:02 -0300

    Filtrando Listas

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,18 +1,21 @@
 import { peopleList } from "@/data/peopleList";
 
 const Page = () => {
+  
+  const chemists = peopleList.filter(person => person.profession === 'chemist');
 
   return (
 
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
-      {peopleList.length > 0 && 
-        <ul>
-          {peopleList.map(person => 
-            <li key={person.id}>{person.name} - {person.profession}</li>
-          )}
-        </ul>
+      {chemists.length > 0 &&
+        <>
+          <h3>Lista de químicos:</h3>
+          <ul>
+            {chemists.map(person => <li key={person.id}>{person.name}</li>)}
+          </ul>
+        </>
       }
     </div>
 
