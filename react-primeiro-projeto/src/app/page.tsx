c4fdcfad6d0a2f68464ba3a5592d9e32b3b6299a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-01-13 18:08:39 -0300

    Renderização Condicional

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -5,13 +5,7 @@ function Page() {
   return <>
     <h1 className="font-bold text-2xl">Olá, mundo!</h1>
     <h3>Algum outro texto!</h3>
-    <Card>
-      <>
-        <h3 className="text-3xl font-bold italic">"Frase de Efeito"</h3>
-        <p className="text-right text-sm">Autor desconhecido</p>
-        <Circle />
-      </>
-    </Card>
+    <Card phrase="Alguma frase TOP" author="Algum cara" />
   </>;
   
 }
